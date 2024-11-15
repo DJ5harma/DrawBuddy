a2a92bd852057ca512f35fa1f4cf980d68ed33fb
DJ5harma <sunubhargav2004@gmail.com>
2024-11-15 10:35:25 +0530

    added continuous multiple lines on click

--- client/src/handlers/LineHandler.tsx
+++ client/src/handlers/LineHandler.tsx
@@ -8,18 +8,42 @@ export default function LineHandler() {
 	const [startingPosition, setStartingPosition] = useState({ x: 0, y: 0 });
 	const [drawing, setDrawing] = useState(false);
 
+	const [multipleLines, setMultipleLines] = useState<{
+		exist: boolean;
+		pointsArr: number[];
+	}>({
+		exist: false,
+		pointsArr: [],
+	});
+
 	const { selectedTool } = useTools();
 	const { addElementToStage } = useStage();
 
 	const handleMouseDown = (e: MouseEvent) => {
-		setDrawing(true);
 		const { clientX: x, clientY: y } = e;
+		if (multipleLines.exist) {
+			setMultipleLines((p) => ({ ...p, pointsArr: [...p.pointsArr, x, y] }));
+			return;
+		}
+		setDrawing(true);
 		setStartingPosition({ x, y });
 	};
 	const handleMouseMove = (e: MouseEvent) => {
 		if (!drawing) return;
 
 		const { clientX: x, clientY: y } = e;
+		if (multipleLines.exist) {
+			setNewLine(
+				<Line
+					key={Math.random()}
+					points={[...multipleLines.pointsArr, x, y]}
+					strokeEnabled
+					strokeWidth={4}
+					stroke={"red"}
+				/>
+			);
+			return;
+		}
 		setNewLine(
 			<Line
 				key={Math.random()}
@@ -30,11 +54,37 @@ export default function LineHandler() {
 			/>
 		);
 	};
-	const handleMouseUp = () => {
+	const handleMouseUp = (e: MouseEvent) => {
+		const { clientX: x, clientY: y } = e;
+		if (multipleLines.exist) return;
+		if (x === startingPosition.x && y === startingPosition.y) {
+			setMultipleLines({ pointsArr: [x, y], exist: true });
+			return;
+		}
+
 		const newElem = NewLine;
 		setNewLine(null);
 		addElementToStage(newElem);
 		setDrawing(false);
+	};
+
+	const handleKeyUp = (e: KeyboardEvent) => {
+		if (multipleLines.exist && e.key === "Escape") {
+			setDrawing(false);
+			setNewLine(null);
+			const pointsArr = multipleLines.pointsArr;
+			setMultipleLines({ exist: false, pointsArr: [] });
+			addElementToStage(
+				<Line
+					key={Math.random()}
+					points={pointsArr}
+					strokeEnabled
+					strokeWidth={4}
+					stroke={"red"}
+				/>
+			);
+			return;
+		}
 	};
 
 	useEffect(() => {
@@ -42,13 +92,15 @@ export default function LineHandler() {
 			document.addEventListener("mousedown", handleMouseDown);
 			document.addEventListener("mousemove", handleMouseMove);
 			document.addEventListener("mouseup", handleMouseUp);
+			document.addEventListener("keyup", handleKeyUp);
 			return () => {
 				document.removeEventListener("mousedown", handleMouseDown);
 				document.removeEventListener("mousemove", handleMouseMove);
 				document.removeEventListener("mouseup", handleMouseUp);
+				document.removeEventListener("keyup", handleKeyUp);
 			};
 		}
-	}, [drawing, startingPosition, NewLine, selectedTool]);
+	}, [drawing, startingPosition, NewLine, selectedTool, multipleLines]);
 
 	return NewLine;
 }
