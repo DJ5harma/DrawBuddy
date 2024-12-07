34de6a10b0c4238e2e7aa109fd79c81c149d590a
DJ5harma <sunubhargav2004@gmail.com>
2024-12-08 03:37:40 +0530

    dragging works consistently now, but still is unfinished

--- client/src/handlers/functional/MainElementsRenderer.tsx
+++ client/src/handlers/functional/MainElementsRenderer.tsx
@@ -3,136 +3,122 @@ import { useElements } from "../../providers/ElementsProvider";
 import { useStage } from "../../providers/StageProvider";
 import { useTools } from "../../providers/ToolsProvider";
 import { useSocket } from "../../providers/SocketProvider";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
+import { deserializeKonvaElement } from "../../utils/konva/convertKonva";
 
 export default function MainElementsRenderer() {
 	const { socket } = useSocket();
 
 	const { selectedToolRef } = useTools();
 
-	const { elementsRef, removeElementFromStage, roomId } = useElements();
+	const { elementsRef, addElementToStage, removeElementFromStage, roomId } =
+		useElements();
 
-	const { getMousePos } = useStage();
+	const { getMousePos, stagePos, stageScale } = useStage();
 
-	const posRef = useRef({ x: 0, y: 0 });
+	const [draggingShape, setDraggingShape] = useState<JSX.Element | null>(null);
 
 	useEffect(() => {
-		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mousemove", handleDragMove);
+		document.addEventListener("mouseup", handleDragEnd);
 		return () => {
-			document.removeEventListener("mousemove", handleMouseMove);
+			document.removeEventListener("mousemove", handleDragMove);
+			document.removeEventListener("mouseup", handleDragEnd);
 		};
-	}, []);
+	}, [draggingShape]);
 
-	const handleMouseMove = (e: MouseEvent) => {
-		posRef.current = getMousePos(e.clientX, e.clientY);
+	const handleDragMove = (e: MouseEvent) => {
+		if (!draggingShape) return;
+		const { x, y } = getMousePos(e.clientX, e.clientY);
+
+		const shape = {
+			...draggingShape,
+			props: { ...draggingShape.props, x, y },
+		};
+
+		setDraggingShape(shape);
+
+		socket.emit("creating_new_element", {
+			element: shape,
+			roomId,
+		});
+	};
+	const handleDragEnd = (e: MouseEvent) => {
+		if (!draggingShape) return;
+
+		const { x, y } = getMousePos(e.clientX, e.clientY);
+
+		const newShape = {
+			...draggingShape,
+			props: { ...draggingShape.props, x, y },
+		};
+
+		addElementToStage({ shape: newShape, stagePos, stageScale }, false);
+
+		socket.emit("creating_new_element", {
+			element: null,
+			roomId,
+		});
+
+		setDraggingShape(null);
 	};
 
-	return [...elementsRef.current.values()].map((element) => {
-		const { shape } = element;
-		if (!shape || !shape.key) return null;
+	return (
+		<>
+			{[...elementsRef.current.values()].map((element) => {
+				const { shape } = element;
+				if (!shape || !shape.key) return null;
 
-		const { key } = shape;
+				const { key } = shape;
 
-		const { type, props } = shape;
+				const { type, props } = shape;
 
-		const handleDragStart = () => {
-			socket.emit("removed_element", {
-				key,
-				roomId,
-			});
-			socket.emit("creating_new_element", {
-				element: shape,
-				roomId,
-			});
-		};
-
-		const handleDragMove = () => {
-			socket.emit("creating_new_element", {
-				element: {
-					...shape,
-					props: {
-						...shape.props,
-						...posRef.current,
-					},
-				},
-				roomId,
-			});
-			// console.log(posRef.current, shape.props.x, shape.props.y);
-		};
-
-		const handleDragEnd = () => {
-			socket.emit("finalized_new_element", {
-				element: {
-					...element,
-					shape: {
-						...shape,
-						props: {
-							...shape.props,
-							...posRef.current,
-						},
-					},
-				},
-				roomId,
-			});
-			socket.emit("creating_new_element", {
-				element: null,
-				roomId,
-			});
-		};
-
-		const draggable = selectedToolRef.current.name === "Pointer";
-
-		switch (type) {
-			case "Rect":
-				return (
-					<Rect
-						key={key}
-						{...props}
-						onClick={() => removeElementFromStage(key, false)}
-						draggable={draggable}
-						onDragStart={handleDragStart}
-						onDragMove={handleDragMove}
-						onDragEnd={handleDragEnd}
-					/>
-				);
-			case "Circle":
-				return (
-					<Circle
-						key={key}
-						{...props}
-						onClick={() => removeElementFromStage(key, false)}
-						draggable={draggable}
-						onDragStart={handleDragStart}
-						onDragMove={handleDragMove}
-						onDragEnd={handleDragEnd}
-					/>
-				);
-			case "Line":
-				return (
-					<Line
-						key={key}
-						{...props}
-						onClick={() => removeElementFromStage(key, false)}
-						draggable={draggable}
-						onDragStart={handleDragStart}
-						onDragMove={handleDragMove}
-						onDragEnd={handleDragEnd}
-					/>
-				);
-			case "Text":
-				return (
-					<Text
-						key={key}
-						{...props}
-						onClick={() => removeElementFromStage(key, false)}
-						draggable={draggable}
-						onDragStart={handleDragStart}
-						onDragMove={handleDragMove}
-						onDragEnd={handleDragEnd}
-					/>
-				);
-			default:
-				return null;
-		}
-	});
+				const handleDragStart = () => {
+					if (selectedToolRef.current.name !== "Pointer" || !key) return;
+					setDraggingShape(shape);
+				};
+				switch (type) {
+					case "Rect":
+						return (
+							<Rect
+								key={key}
+								{...props}
+								onClick={() => removeElementFromStage(key, false)}
+								onMouseDown={handleDragStart}
+							/>
+						);
+					case "Circle":
+						return (
+							<Circle
+								key={key}
+								{...props}
+								onClick={() => removeElementFromStage(key, false)}
+								onMouseDown={handleDragStart}
+							/>
+						);
+					case "Line":
+						return (
+							<Line
+								key={key}
+								{...props}
+								onClick={() => removeElementFromStage(key, false)}
+								onMouseDown={handleDragStart}
+							/>
+						);
+					case "Text":
+						return (
+							<Text
+								key={key}
+								{...props}
+								onClick={() => removeElementFromStage(key, false)}
+								onMouseDown={handleDragStart}
+							/>
+						);
+					default:
+						return null;
+				}
+			})}
+			{draggingShape ? deserializeKonvaElement(draggingShape) : null}
+		</>
+	);
 }
