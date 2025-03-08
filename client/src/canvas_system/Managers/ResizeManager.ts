85f4c22e5f673b62fd999630803cb7a71dc447fd
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2025-03-08 17:38:43 +0530

    added resizing code but is buggy

--- client/src/canvas_system/Managers/ResizeManager.ts
+++ client/src/canvas_system/Managers/ResizeManager.ts
@@ -1,10 +1,12 @@
-import { Shape } from "../Shapes/Shape";
 import { CanvasManager } from "./CanvasManager";
 import { SelectionDragManager } from "./SelectionDragManager";
 import { SelectionManager } from "./SelectionManager";
 
 let starting_pos: vec2 = [0, 0];
-let resizing_shape: Shape | undefined = undefined;
+
+let resizing = false;
+
+const shape_to_resize_handle_map = new Map<ImageDataObj, ResizeHandle>();
 
 export class ResizeManager {
     public static init() {
@@ -15,40 +17,77 @@ export class ResizeManager {
         document.addEventListener("mouseup", this.mouseup);
     }
 
-    private static start_interaction(_touch_pos: vec2, shape: Shape): void {
+    private static get_handle(_touch_pos: vec2, shape: ImageDataObj) {
         console.log("setting dir");
         const [x, y] = _touch_pos;
 
-        if (shape.bounding_rect) {
-            document.body.style.cursor = "e-resize";
-            const left = shape.bounding_rect.top_left[0];
-            const right = shape.bounding_rect.bottom_right[0];
-            const top = shape.bounding_rect.top_left[1];
-            const bottom = shape.bounding_rect.bottom_right[1];
-            if (x - left <= 10 && y - top <= 10) {
-                shape.resize_handle = "nw";
-            } else if (x - left <= 10 && bottom - y <= 10) {
-                shape.resize_handle = "sw";
-            } else if (right - x <= 10 && y - top <= 10) {
-                shape.resize_handle = "ne";
-            } else if (right - x <= 10 && bottom - y <= 10) {
-                shape.resize_handle = "se";
-            } else if (x - left <= 10) {
-                shape.resize_handle = "w";
-            } else if (y - top <= 10) {
-                shape.resize_handle = "n";
-            } else if (right - x <= 10) {
-                shape.resize_handle = "e";
-            } else if (bottom - y <= 10) {
-                shape.resize_handle = "s";
+        let handle: ResizeHandle | undefined = undefined;
+
+        document.body.style.cursor = "e-resize";
+        const left = shape.bounding_rect.top_left[0];
+        const right = shape.bounding_rect.bottom_right[0];
+        const top = shape.bounding_rect.top_left[1];
+        const bottom = shape.bounding_rect.bottom_right[1];
+
+        if (x - left <= 10 && y - top <= 10) handle = "nw";
+        else if (x - left <= 10 && bottom - y <= 10) handle = "sw";
+        else if (right - x <= 10 && y - top <= 10) handle = "ne";
+        else if (right - x <= 10 && bottom - y <= 10) handle = "se";
+        else if (x - left <= 10) handle = "w";
+        else if (y - top <= 10) handle = "n";
+        else if (right - x <= 10) handle = "e";
+        else if (bottom - y <= 10) handle = "s";
+
+        if (!handle) console.error("handle for shape not found", shape);
+
+        console.log("setting dir to :", handle);
+
+        return handle || "n";
+    }
+
+    private static resize_shapes_by(delta: vec2) {
+        const [dx, dy] = delta;
+        for (const [shape, handle] of shape_to_resize_handle_map) {
+            let dl = 0;
+            let dr = 0;
+            let dn = 0;
+            let ds = 0;
+
+            for (let i = 0; i < handle.length; ++i) {
+                switch (handle[i]) {
+                    case "e": {
+                        dr += dx;
+                        break;
+                    }
+                    case "s": {
+                        ds += dy;
+                        break;
+                    }
+                    case "w": {
+                        dl += dx;
+                        break;
+                    }
+                    case "n": {
+                        dn += dy;
+                        break;
+                    }
+                }
             }
-            console.log("setting dir to :", shape.resize_handle);
+            shape.sx += dl;
+            shape.sy += dn;
+
+            shape.bounding_rect.top_left[0] += dl;
+            shape.bounding_rect.top_left[1] += dn;
+            shape.bounding_rect.bottom_right[0] += dr;
+            shape.bounding_rect.bottom_right[1] += ds;
+
+            CanvasManager.clear_canvas_only_unrender().render_stored_shapes_all();
+            SelectionManager.unrender_selection_of_all().render_selection_of_all();
         }
     }
 
     private static mousedown(e: MouseEvent) {
         if (SelectionDragManager.is_dragging()) return;
-
         const selected_shapes = [...SelectionManager.get_selected_shapes()];
 
         // if(selected_shapes.length > 1) return; // If we desire selection to restrict to one only
@@ -57,41 +96,41 @@ export class ResizeManager {
         const abs = Math.abs;
 
         for (let i = selected_shapes.length - 1; i != -1; --i) {
-            const shape = selected_shapes[i];
+            const { sx, sy, bounding_rect } = selected_shapes[i];
 
-            if (!shape.bounding_rect) return;
-
-            const { top_left, bottom_right } = shape.bounding_rect;
+            const { top_left, bottom_right } = bounding_rect;
 
             const diffs = [
-                abs(top_left[0] - x), // left
-                abs(bottom_right[0] - x), // right
-                abs(top_left[1] - y), // top
-                abs(bottom_right[1] - y), // bottom
+                abs(top_left[0] + sx - x), // left
+                abs(bottom_right[0] + sx - x), // right
+                abs(top_left[1] + sy - y), // top
+                abs(bottom_right[1] + sy - y), // bottom
             ];
 
             diffs.forEach((d) => {
                 if (d < 10) {
+                    resizing = true;
                     selected_shapes.forEach((shp) => {
-                        ResizeManager.start_interaction([x, y], shp);
+                        shape_to_resize_handle_map.set(
+                            shp,
+                            ResizeManager.get_handle([x, y], shp)
+                        );
                     });
-                    resizing_shape = shape;
-                    starting_pos = [x, y];
                     return;
                 }
             });
+
+            starting_pos = [x, y];
         }
     }
 
     private static mousemove(e: MouseEvent) {
-        if (!resizing_shape) return;
+        if (!resizing) return;
 
-        SelectionManager.get_selected_shapes().forEach((shape) =>
-            shape.resize_by([
-                e.clientX - starting_pos[0],
-                e.clientY - starting_pos[1],
-            ])
-        );
+        ResizeManager.resize_shapes_by([
+            e.clientX - starting_pos[0],
+            e.clientY - starting_pos[1],
+        ]);
 
         starting_pos = [e.clientX, e.clientY];
 
@@ -100,9 +139,7 @@ export class ResizeManager {
     }
 
     private static mouseup(_: MouseEvent) {
-        if (!resizing_shape) return;
-        resizing_shape.fix_maths();
-        resizing_shape = undefined;
+        resizing = false;
 
         document.body.style.cursor = "default";
     }
--- client/src/canvas_system/Shapes/Rectangle.ts
+++ client/src/canvas_system/Shapes/Rectangle.ts
@@ -25,31 +25,6 @@ export class Rectangle implements Shape {
         ctx.closePath();
     }
 
-    public is_inside_rect(_rect: { pos: vec2; dims: vec2 }): boolean {
-        const { pos, dims } = _rect;
-
-        return (
-            pos[0] < this.pos[0] &&
-            pos[1] < this.pos[1] &&
-            pos[0] + dims[0] > this.pos[0] + this.dims[0] &&
-            pos[1] + dims[1] > this.pos[1] + this.dims[1]
-        );
-    }
-
-    public displace_by(_displacement: vec2): void {
-        const [x, y] = _displacement;
-
-        this.pos[0] += x;
-        this.pos[1] += y;
-
-        if (this.bounding_rect) {
-            this.bounding_rect.top_left[0] += x;
-            this.bounding_rect.bottom_right[0] += x;
-            this.bounding_rect.top_left[1] += y;
-            this.bounding_rect.bottom_right[1] += y;
-        }
-    }
-
     public get_bounding_rect(): BoundingRect {
         let [x, y] = this.pos;
         let [w, l] = this.dims;
@@ -75,60 +50,47 @@ export class Rectangle implements Shape {
         };
     }
 
-    public resize_by(delta_xy: vec2) {
-        const [x, y] = delta_xy;
+    // public resize_by(delta_xy: vec2) {
+    //     const [x, y] = delta_xy;
 
-        const str = this.resize_handle;
+    //     const str = this.resize_handle;
 
-        if (!this.bounding_rect) {
-            console.error("BR not found");
-            return;
-        }
+    //     if (!this.bounding_rect) {
+    //         console.error("BR not found");
+    //         return;
+    //     }
 
-        if (!str) {
-            console.error("Resize handle not found");
-            return;
-        }
+    //     if (!str) {
+    //         console.error("Resize handle not found");
+    //         return;
+    //     }
 
-        for (let i = 0; i < str.length; ++i) {
-            switch (str[i]) {
-                case "e": {
-                    this.dims[0] += x;
-                    this.bounding_rect.bottom_right[0] += x;
-                    break;
-                }
-                case "s": {
-                    this.dims[1] += y;
-                    this.bounding_rect.bottom_right[1] += y;
-                    break;
-                }
-                case "w": {
-                    this.dims[0] -= x;
-                    this.bounding_rect.top_left[0] += x;
-                    this.pos[0] += x;
+    //     for (let i = 0; i < str.length; ++i) {
+    //         switch (str[i]) {
+    //             case "e": {
+    //                 this.dims[0] += x;
+    //                 this.bounding_rect.bottom_right[0] += x;
+    //                 break;
+    //             }
+    //             case "s": {
+    //                 this.dims[1] += y;
+    //                 this.bounding_rect.bottom_right[1] += y;
+    //                 break;
+    //             }
+    //             case "w": {
+    //                 this.dims[0] -= x;
+    //                 this.bounding_rect.top_left[0] += x;
+    //                 this.pos[0] += x;
 
-                    break;
-                }
-                case "n": {
-                    this.dims[1] -= y;
-                    this.bounding_rect.top_left[1] += y;
-                    this.pos[1] += y;
-                    break;
-                }
-            }
-        }
-    }
-    public zoom_by(_d: number): void {
-        this.pos[0] -= _d;
-        this.pos[1] -= _d;
-
-        if (this.dims[0] - _d < 10 || this.dims[1] - _d < 10) return;
-
-        this.dims[0] -= _d;
-        this.dims[1] -= _d;
-
-        // this.fix_maths();
-
-        return;
-    }
+    //                 break;
+    //             }
+    //             case "n": {
+    //                 this.dims[1] -= y;
+    //                 this.bounding_rect.top_left[1] += y;
+    //                 this.pos[1] += y;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
 }
